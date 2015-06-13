c10cc7e960099e5e5dcf0f366dc8dacfd62ca3a4
Patrick Hobusch <patrick.hobusch@gmail.com>
2015-06-13 22:31:28 +0200

    Implement basic rest api.

---
+++ sources/controller/api.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once(realpath('core/class.restapi.php'));
+// require_once(realpath('core/lib.xml.php'));
+
+// Requests from the same server don't have a HTTP_ORIGIN header
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+    $api = new RestAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $api->processRequest();
+} catch (Exception $e) {
+    echo json_encode(Array('Error' => $e->getMessage()));
+}
+
+?>
---
+++ sources/controller/model/class.quiz.php
@@ -0,0 +1,10 @@
+<?php
+
+class Quiz {
+	
+    public function GET($args, $verb) {
+		return $verb." kommt an in Klasse Quiz";
+	}
+}
+
+?>
