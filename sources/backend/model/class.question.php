9d1473029f236a7649ae113ab2fb79f075ca4a3e
Kim Kern <kim.kern@fu-berlin.de>
2015-07-07 19:29:26 +0200

    add new dummy questions+pics, remove some old ones

--- sources/backend/model/class.question.php
+++ sources/backend/model/class.question.php
@@ -73,20 +73,11 @@ class QuestionDao extends BaseDao {
 
             $questions = array(
                 new Question(
-                    'Aus welchem Land stammt dieses Bild?',
-                    'Der Autor ist im jahre 1881 geboren.',
-                    'img/picasso_1.jpg',
-                    'Spanien',
-                    'Deutschland',
-                    'Lettland',
-                    'Argentinien'
-                ),
-                new Question(
                     'Welchen Titel hat dieses Bild?',
                     'Das Bild ist von Pablo Picasso.',
                     'img/boy_leading_horse.jpg',
                     'Boy Leading a Horse',
-                    'Femme aux Bras Croisés',
+                    'Femme aux Bras Croisï¿½s',
                     'Family of Saltimbanques',
                     'oil on canvas'
                 ),
@@ -100,13 +91,121 @@ class QuestionDao extends BaseDao {
                     '1944'
                 ),
                 new Question(
-                    'Welche Besonderheit hat dieses Bild?',
-                    'Das Bild ist von Pablo Picasso.',
-                    'img/oil_painting.jpg',
-                    'Öl auf Leinwand',
-                    'Öl auf Holz',
-                    'Pastellkreiden auf Karton',
-                    'Wachsfarben auf Holz'
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Adam Elsheimer.',
+                    'img/fluchtnachaegypten.jpg',
+                    '1609',
+                    '1902',
+                    '1822',
+                    '1855'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Albrecht Altdorfer.',
+                    'img/altdorfer.jpg',
+                    '1520',
+                    '1944',
+                    '1609',
+                    '1960'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von Alexandre Cabanel.',
+                    'img/venus_birth.jpg',
+                    'The Birth of Venus',
+                    'San Zeno Altarpiece',
+                    'Parnassus',
+                    'St. Bernardino of Siena between Two Angels'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von Andrea Mantegna.',
+                    'img/andrea.jpg',
+                    'Portrait of Francesco Gonzaga',
+                    'Presentation at the Temple',
+                    'Triumph of the Virtues',
+                    'St. Bernardino of Siena between Two Angels'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von Andrea Mantegna.',
+                    'img/madonna.jpg',
+                    'The Madonna of the Cherubim',
+                    'Christ as the Suffering Redeemer',
+                    'Madonna della Vittoria',
+                    'Death of the Virgin'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von Fra Angelico.',
+                    'img/last_judgment.jpg',
+                    'The Last Judgment',
+                    'Coronation of the Virgin',
+                    'Landscape with the Flight into Egypt',
+                    'Napoleon as Mars the Peacemaker'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von Camille Pissarro.',
+                    'img/the_house.jpg',
+                    'The House of the Deaf Woman and the Belfry at Eragny',
+                    'MaestÃ ',
+                    'Lipstick (Ascending) on Caterpillar Tracks',
+                    'The Button'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Claude Lorrain.',
+                    'img/claude.jpg',
+                    '1635',
+                    '1873',
+                    '1732',
+                    '1868'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Marcel Duchamp.',
+                    'img/duchamp.jpg',
+                    '1912',
+                    '1602',
+                    '1966',
+                    '1598'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Masaccio.',
+                    'img/massacio.jpg',
+                    '1425',
+                    '1872',
+                    '1594',
+                    '1528'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Umberto Boccioni.',
+                    'img/umberto.jpg',
+                    '1910',
+                    '1571',
+                    '1624',
+                    '1428'
+                ),
+                new Question(
+                    'Aus welchem Jahr stammt dieses Bild?',
+                    'Das Bild ist von Vincent van Gogh.',
+                    'img/vase.jpg',
+                    '1890',
+                    '1820',
+                    '1624',
+                    '1902'
+                ),
+                new Question(
+                    'Welchen Titel hat dieses Bild?',
+                    'Das Bild ist von William Blake.',
+                    'img/andrea.jpg',
+                    'Pity',
+                    'The Bench',
+                    'The Distrest Poet',
+                    'Bulb Fields'
                 ),
             );
 
