1ef0cfa2389cd22ea68d2d7c9edf01d23ce9b817
Kim Kern <kim.kern@fu-berlin.de>
2015-07-07 22:02:08 +0200

    add artwork-year-query

--- sources/backend/model/class.artwork.php
+++ sources/backend/model/class.artwork.php
@@ -12,18 +12,26 @@ class Artwork extends Base {
 
 class ArtworkDao extends BaseDao {
 
-    public static $Database = '/var/www/backend/db/artwork_database.xml';
+    public static $ArtworksDatabase = '/var/www/backend/db/artworks_database.xml';
+    public static $PersonsDatabase = '/var/www/backend/db/persons_database.xml';
 
     public function GET($verb, $args) {
 
-        if ($verb == '') {
+        if ($verb == 'yearQuestion') {
 
             $queryStr = "
-                \n for \$artwork in doc('".Artwork::$Database."')//artwork
-                \n let \$name := \$artwork/name/text()
-                \n let \$year := \$artwork/year/text()
-                \n return
-                \n <artwork year='{\$year}'>{\$name}</artwork>
+				for \$artworks in doc(".Artwork::$ArtworksDatabase.")/artworks,
+				  \$persons in doc(".Artwork::$PersonsDatabase.")/persons
+				let \$artwork := \$artworks/artwork[matches(year/text(), '^[0-9][0-9][0-9][0-9]\$')]
+				let \$rows := count(\$artwork)
+				let \$rand0 := random:integer(\$rows -1)+1
+				let \$rand1 := random:integer(\$rows -1)+1
+				let \$rand2 := random:integer(\$rows -1)+1
+				let \$rand3 := random:integer(\$rows -1)+1
+				let \$id := \$artwork[\$rand0]/personID/@ID
+				let \$painter := \$persons/person[personID[@ID=\$id]]/name/text()
+
+				return ('{\"question\":\"Aus welchem Jahr stammt dieses Bild?\",\"hint\":\"Das Bild ist von ',\$painter,'.\",\"image\":\"',\$artwork[\$rand0]/thumbnail/text(),'\",\"answers\":{\"rightAnswer\":',\$artwork[\$rand0]/year/text(),',\"wrongAnswer1\":',\$artwork[\$rand1]/year/text(),',\"wrongAnswer2\":',\$artwork[\$rand2]/year/text(),',\"wrongAnswer3\":',\$artwork[\$rand3]/year/text(),'}}')
             ";
 
             $query = $this->_zorba->compileQuery($queryStr);
