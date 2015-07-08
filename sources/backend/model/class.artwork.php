c7a37516294f94781ad9d59dfc6adf766ff0a9c0
Kim Kern <kim.kern@fu-berlin.de>
2015-07-08 16:05:28 +0200

    change basex query years

--- sources/backend/model/class.artwork.php
+++ sources/backend/model/class.artwork.php
@@ -21,9 +21,9 @@ class ArtworkDao extends BaseDao {
 
             $queryStr = "
 				import module namespace r = \"http://www.zorba-xquery.com/modules/random\";
-				for \$artworks in doc(".ArtworkDao::$ArtworksDatabase.")/artworks,
+				(:for \$artworks in doc(".ArtworkDao::$ArtworksDatabase.")/artworks,
 				  \$persons in doc(".ArtworkDao::$PersonsDatabase.")/persons
-				let \$artwork := \$artworks/artwork(:[matches(year/text(), '^[0-9][0-9][0-9][0-9]\$')]:)
+				let \$artwork := \$artworks/artwork[matches(year/text(), '^[0-9][0-9][0-9][0-9]\$')]
 				let \$rows := count(\$artwork)
 				let \$rand0 := r:random-between(1, \$rows)
 				let \$rand1 := r:random-between(1, \$rows)
@@ -31,7 +31,9 @@ class ArtworkDao extends BaseDao {
 				let \$rand3 := r:random-between(1, \$rows)
 				let \$id := \$artwork[\$rand0]/personID/@ID
 				let \$painter := \$persons/person[personID[@ID=\$id]]/name/text()
-				return ('{\"question\":\"Aus welchem Jahr stammt dieses Bild?\",\"hint\":\"Das Bild ist von ',\$painter,'.\",\"image\":\"',\$artwork[\$rand0]/thumbnail/text(),'\",\"answers\":{\"rightAnswer\":',\$artwork[\$rand0]/year/text(),',\"wrongAnswer1\":',\$artwork[\$rand1]/year/text(),',\"wrongAnswer2\":',\$artwork[\$rand2]/year/text(),',\"wrongAnswer3\":',\$artwork[\$rand3]/year/text(),'}}')
+				return ('{\"question\":\"Aus welchem Jahr stammt dieses Bild?\",\"hint\":\"Das Bild ist von ',\$painter,'.\",\"image\":\"',\$artwork[\$rand0]/thumbnail/text(),'\",\"answers\":{\"rightAnswer\":',\$artwork[\$rand0]/year/text(),',\"wrongAnswer1\":',\$artwork[\$rand1]/year/text(),',\"wrongAnswer2\":',\$artwork[\$rand2]/year/text(),',\"wrongAnswer3\":',\$artwork[\$rand3]/year/text(),'}}'):)
+				for \$artworks in doc(".ArtworkDao::$ArtworksDatabase.")/artworks
+				return r:random-between(1,10)
             ";
 
             $query = $this->_zorba->compileQuery($queryStr);
