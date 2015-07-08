8615e9375c814792ae1e4664feab934fa220d386
oe-bayram <oe-bayram@live.de>
2015-07-08 10:40:36 +0200

    Updated Controller in Frontend

--- sources/frontend/includes/controller.php
+++ sources/frontend/includes/controller.php
@@ -14,68 +14,8 @@ function requestQuestion($SID){
 			)
 	));
 	
-	$response = file_get_contents('http://bilderquiz.hobusch.net/api/question/random', FALSE, $context);
-	
-	
-	if($response === FALSE){
-			die('Error');
-	}
-	
-	/*
-	$response= array('{
-    "request": {
-        "question": "Aus welchem Land stammt dieses Bild?",
-				"hint":"Der Autor ist im jahre 1881 geboren.",
-    		"image":"img/picasso_1.jpg",
-        "answers":{
-        	"rightAnswer":"Spanien",
-          "wrongAnswer1":"Deutschland",
-          "wrongAnswer2":"Lettland",
-          "wrongAnswer3":"Argentinien"
-       	}
-    }
-	}','{
-    "request": {
-        "question": "Welchen Titel hat dieses Bild?",
-				"hint":"Das Bild ist von Pablo Picasso.",
-    		"image":"img/boy_leading_horse.jpg",
-        "answers":{
-        	"rightAnswer":"Boy Leading a Horse",
-          "wrongAnswer1":"Femme aux Bras Croisés",
-          "wrongAnswer2":"Family of Saltimbanques",
-          "wrongAnswer3":"oil on canvas"
-       	}
-    }
-	}','{
-    "request": {
-        "question": "Aus welchem Jahr stammt dieses Bild?",
-				"hint":"Das Bild ist von Pablo Picasso.",
-    		"image":"img/ma_jolie.jpg",
-        "answers":{
-        	"rightAnswer":"1914",
-          "wrongAnswer1":"1925",
-          "wrongAnswer2":"1900",
-          "wrongAnswer3":"1944"
-       	}
-    }
-	}','{
-    "request": {
-        "question": "Welche Besonderheit hat dieses Bild?",
-				"hint":"Das Bild ist von Pablo Picasso.",
-    		"image":"img/oil_painting.jpg",
-        "answers":{
-        	"rightAnswer":"Öl auf Leinwand",
-          "wrongAnswer1":"Öl auf Holz",
-          "wrongAnswer2":"Pastellkreiden auf Karton",
-          "wrongAnswer3":"Wachsfarben auf Holz"
-       	}
-    }
-	}');
-	*/
-	
-	//$random = rand(0,3);
-	
-	//return $response[$random];
+	$response = file_get_contents('http://bilderquiz.hobusch.net/api/question/random');
+
 	return $response;
 }
 
@@ -85,14 +25,14 @@ function getQuestion($SID){
 
 	$questionJSONObject = json_decode($requestQuestion, TRUE);
 	
-	$question = $questionJSONObject['request']['question'];
-	$hint = $questionJSONObject['request']['hint'];
-	$image = $questionJSONObject['request']['image'];
+	$question = $questionJSONObject['question'];
+	$hint = $questionJSONObject['hint'];
+	$image = $questionJSONObject['image'];
 	
-	$rightAnswer = $questionJSONObject['request']['answers']['rightAnswer'];
-	$wrongAnswers = array($questionJSONObject['request']['answers']['wrongAnswer1'], 
-												$questionJSONObject['request']['answers']['wrongAnswer2'], 
-												$questionJSONObject['request']['answers']['wrongAnswer3']);
+	$rightAnswer = $questionJSONObject['answers']['rightAnswer'];
+	$wrongAnswers = array($questionJSONObject['answers']['wrongAnswer1'], 
+												$questionJSONObject['answers']['wrongAnswer2'], 
+												$questionJSONObject['answers']['wrongAnswer3']);
 												
 	$rightAnswerPosition = rand(0,3);
 	$answerButtons[$rightAnswerPosition] = $rightAnswer;
