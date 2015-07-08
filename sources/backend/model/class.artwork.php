0d2f51961ead4500b4aa9b8b7f6dd742080e1901
Kim Kern <kim.kern@fu-berlin.de>
2015-07-08 17:55:51 +0200

    test query

--- sources/backend/model/class.artwork.php
+++ sources/backend/model/class.artwork.php
@@ -20,8 +20,8 @@ class ArtworkDao extends BaseDao {
         if ($verb == 'year') {
 
             $queryStr = "
-				declare option output:omit-xml-declaration \"yes\";
 				import module namespace r = \"http://www.zorba-xquery.com/modules/random\";
+				declare option output:omit-xml-declaration \"yes\";
 				for \$artworks in doc('/var/www/backend/db/artworks_database.xml')/artworks,
 					\$persons in doc('/var/www/backend/db/persons_database.xml')/persons
  				let \$artwork := \$artworks/artwork[matches(year/text(), '^[0-9][0-9][0-9][0-9]\$')]
@@ -32,7 +32,7 @@ class ArtworkDao extends BaseDao {
 				let \$rand3 := r:random-between(1, \$rows)
  				let \$id := \$artwork[\$rand0]/personID/@ID
  				let \$painter := \$persons/person[personID[@ID=\$id]]/name/text()
-				return ('{\"question\":\"Aus welchem Jahr stammt dieses Bild?\",\"hint\":\"Das Bild ist von ',\$painter,'.\",\"image\":\"',\$artwork[\$rand0]/thumbnail/text(),'\",\"answers\":{\"rightAnswer\":',\$artwork[\$rand0]/year/text(),',\"wrongAnswer1\":',\$artwork[\$rand1]/year/text(),',\"wrongAnswer2\":',\$artwork[\$rand2]/year/text(),',\"wrongAnswer3\":',\$artwork[\$rand3]/year/text(),'}}')
+				return ('{\"question\":\"Aus welchem Jahr stammt dieses Bild?\",\"hint\":\"Das Bild ist von ',\$painter,'.\",\"image\":\"',\$artwork[\$rand0]/thumbnail/text(),'\",\"answers\":{\"rightAnswer\":',\$artwork[\$rand0]/year/text(),',\"wrongAnswer1\":',\$artwork[\$rand1]/year/text(),',\"wrongAnswer2\":',\$artwork[\$rand2]/year/text(),',\"wrongAnswer3\":',\$artwork[\$rand3]/year/text(),'},\"wikilink\":\"',\$artwork[\$rand0]/wikilink/text(),'\"}')
          
             ";
 
